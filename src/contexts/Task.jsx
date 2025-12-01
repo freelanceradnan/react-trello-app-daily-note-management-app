4edc58b6b511e45c82793e20124462403f58ac65
freelanceradnan <reactorbro722@gmail.com>
2025-12-01 22:47:55 +0600

    board delete added deleted  list and taks item similer board

--- src/contexts/Task.jsx
+++ src/contexts/Task.jsx
@@ -1,14 +1,14 @@
 import { createContext,useReducer} from "react";
 import { listReducer } from "../reducers/Lists";
 
-export const ListContext=createContext()
+export const TaskContext=createContext()
 
 const ListProvider=({children})=>{
-    const [lists,dispatchListAction]=useReducer(listReducer,[])
+    const [lists,dispatchTaskAction]=useReducer(listReducer,[])
     return(
-        <ListContext.Provider value={{lists,dispatchListAction}}>
+        <TaskContext.Provider value={{lists,dispatchTaskAction}}>
             {children}
-        </ListContext.Provider>
+        </TaskContext.Provider>
     )
 }
 export default ListProvider
--- src/pages/BoardItem.jsx
+++ src/pages/BoardItem.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useContext } from 'react';
+import { BoardContext } from '../contexts/Board';
+import { ListContext } from '../contexts/List';
+import { TaskContext } from '../contexts/Task';
 
 const BoardItem = ({board}) => {
+    const {dispatchBoardActions}=useContext(BoardContext)
+    const {dispatchListAction}=useContext(ListContext)
+    const {dispatchTaskAction}=useContext(TaskContext)
+    const removeHandler=(e)=>{
+        e.preventDefault()
+        e.stopPropagation()
+dispatchBoardActions({type:"REMOVE_BOARD",payload:board.id})
+board.lists.forEach((listId)=>{
+    dispatchListAction({type:"REMOVE_LIST",payload:listId})
+})
+board.tasks.forEach((taskId)=>{
+    dispatchTaskAction({type:"REMOVE_TASK",payload:taskId})
+})
+    }
     return (
         <div>
             <h5>{board.title}</h5>
-            <p>x</p>
+            <p onClick={removeHandler}>x</p>
         </div>
     );
 };
